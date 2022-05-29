f507ab34d6dcd61839cf1245bcfe7030355ee6e5
Prabhat <prabhat8k@gmail.com>
2022-05-29 12:14:45 +0530

    test

--- model.py
+++ model.py
@@ -1,2 +1,11 @@
 from pycaret.classification import *
-exp_mclf101 = setup(data = data, target = 'species', session_id=123) 
+import pandas as pd
+base_df3_15 = pd.read_csv(r'C:\Users\8prab\Google Drive\Work\trading\database_df3_15.csv')
+base_df3_15.index = pd.DatetimeIndex(base_df3_15.index)
+base_df3_15 = base_df3_15.sort_index()
+base_df3_15 = base_df3_15[~base_df3_15.index.duplicated(keep='first')]
+print(base_df3_15)
+print(base_df3_15.dtypes)
+exp_mclf101 = setup(data = base_df3_15, target = 'label_-15_1min_close', session_id=123) 
+best = compare_models()
+
