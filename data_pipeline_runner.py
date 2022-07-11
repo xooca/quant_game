36b3fbaf74b168bf034decfc5e0cafadea3c985c
Prabhat <prabhat8k@gmail.com>
2022-07-11 21:16:59 +0530

    refactored and created new config directory

--- data_pipeline_runner.py
+++ data_pipeline_runner.py
@@ -10,8 +10,12 @@ import data.data_pipelines as dd
 import data.data_utils as du
 
 base_df = pd.read_csv(dd.dc.data.paths.input_path)
+
+
 all_func = dd.__dict__
 for datapipeline,subdatapipeline in dd.dc.data.datapipeline.items():
     all_pipe = [all_func[pipe] for pipe in subdatapipeline]
     print(all_pipe)
-    df = du.run_pipeline(all_pipe,base_df,dd.dc.data.paths.pipeinfo,f"dd.dc.data.paths.base_data_loc{datapipeline}.csv",load_previous=False)
+    print(f"{dd.dc.data.paths.base_data_loc}{datapipeline}.pkl")
+    print(f"{dd.dc.data.paths.base_data_loc}{datapipeline}.csv")
+    df = du.run_pipeline(all_pipe,base_df,f"{dd.dc.data.paths.base_data_loc}{datapipeline}.pkl",f"{dd.dc.data.paths.base_data_loc}{datapipeline}.csv",load_previous=False)
--- modelling/model_autogluon.py
+++ modelling/model_autogluon.py
@@ -1,14 +1,32 @@
 class modelling:
-    def __init__(self,config,df):
+    def __init__(self,config):
         self.config = config
 
-    def optimize(self,df):
+    def define_dataset(self,load_test=False):
+        from autogluon.tabular import TabularDataset
+        self.train = TabularDataset(self.config.data.path.train_save_path)
+        self.valid = TabularDataset(self.config.data.path.valid_save_path)
+        if load_test:
+            self.test = TabularDataset(self.config.data.path.test_save_path)
+        
+    def define_hyperparameters(self):
+        self.hyperparameters = self.config.autogluon.autogluon.optimization.hyperparamters
+
+    def optimize(self):
+        from autogluon.tabular import TabularPredictor
+        import autogluon.core as ag
+
+        hyperparameter_tune_kwargs =self.config.autogluon.autogluon.optimization.hyperparameter_tune_kwargs
+        self.predictor = TabularPredictor(label=self.config.autogluon.autogluon.model_metadata.target_column, eval_metric=self.config.autogluon.autogluon.evaluation.eval_metric).fit(
+        self.train, tuning_data=self.valid, time_limit=self.config.autogluon.autogluon.optimization.generic.time_limit,
+        hyperparameters=hyperparameters, hyperparameter_tune_kwargs=hyperparameter_tune_kwargs,
+)
 
     def train(self,df):
         import autogluon.core as ag
         from autogluon.tabular import TabularDataset, TabularPredictor
-        train_data = TabularDataset(self.config.autogluon.optimization.training_data_path)
-        val_data = TabularDataset(self.config.autogluon.optimization.validation_data_path)
+        self.train = TabularDataset(self.config.data.path.train_save_path)
+        self.valid = TabularDataset(self.config.data.path.valid_save_path)
 
     def evaluate(self,df):
 
